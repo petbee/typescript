e96face33959fc6949a7466c49740d2c78b5e5c4
Mateus Saggin <mateus@saggin.com.br>
2025-05-22 22:15:43 -0300

    Fix nestjs eslint 8

--- packages/eslint-config/rules/nestjs.js
+++ packages/eslint-config/rules/nestjs.js
@@ -3,7 +3,11 @@ const nestjsLint = require('@darraghor/eslint-plugin-nestjs-typed')
 
 const hasNestJs = hasPackage('@nestjs/core')
 
-module.exports = hasNestJs ? nestjsLint.classicPlugin : {}
+module.exports = hasNestJs
+  ? {
+      plugins: [nestjsLint.classicPlugin],
+    }
+  : {}
 
 // Flat config for ESLint v9 (no extends, plugins as object)
 module.exports.flat = hasNestJs ? nestjsLint.default.configs.flatRecommended : []
